d88131cb31c10105777ff6e911a3da312330f5b9
Naru Koshin <k@narukoshin.me>
2024-03-09 17:55:54 +0200

    added keywords and desc

--- hw3/frontend/partials/header.php
+++ hw3/frontend/partials/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$web->title?></title>
-    <meta name="description" content="">
-    <meta name="keywords" content="">
+    <meta name="description" content="Explore the beauty and culture of Japan with our comprehensive travel guide. Discover top destinations, attractions, cuisine, and more.">
+    <meta name="keywords" content="Japan, travel, japan national food, national food, guide, tourism, Tokyo, Kyoto, Osaka, culture, traditions, temples, shrines, cuisine, sushi, ramen, onsen, cherry blossoms, Mount Fuji, attractions, sightseeing">
     <meta name="author" content="Alekss B.Briģis, Jana Liekniņa">
     <link href="frontend/assets/css/halfmoon.min.css" rel="stylesheet">
     <link href="frontend/assets/css/halfmoon.modern.min.css" rel="stylesheet">
